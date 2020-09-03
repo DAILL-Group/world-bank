65526da067ec237044077e2bfd7cf6d5247eee51
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-09-03 22:18:42 +0200

    Created Server.js

---
+++ Server.js
@@ -0,0 +1,23 @@
+// default dependancies
+
+var fs = require("fs");
+var net = require("net");
+
+
+// default files
+
+
+
+
+// default datas
+
+
+
+
+// Server varaible
+
+var server = net.createServer(function (socket) {
+     
+     
+     
+});
